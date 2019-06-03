1b799143c3c496503007c9fa339d716d862f105b
SuicidalLabRat <suicidallabrat@gmail.com>
2019-06-03 15:54:13 -0700

    Moved variables out of the class functions and into main.

--- python/paho/pub_sub_test.py
+++ python/paho/pub_sub_test.py
@@ -19,6 +19,20 @@ class PubSub(object):
         self.topic = topic
         self.logger = logging.getLogger(repr(self))
 
+    def ssl_alpn(self, ca='rootCa', cert='client.crt', key='client.key'):
+        iot_protocol_name = "x-amzn-mqtt-ca"
+        try:
+            # debug print opnessl version
+            self.logger.info("open ssl version:{}".format(ssl.OPENSSL_VERSION))
+            ssl_context = ssl.create_default_context()
+            ssl_context.set_alpn_protocols([iot_protocol_name])
+            ssl_context.load_verify_locations(cafile=ca)
+            ssl_context.load_cert_chain(certfile=cert, keyfile=key)
+            return ssl_context
+        except Exception as e:
+            print("exception ssl_alpn()")
+            raise e
+
     def __on_connect(self, client, userdata, flags, rc):
         self.connect = True
         if self.listener:
@@ -44,44 +58,39 @@ class PubSub(object):
     def __on_log(self, client, userdata, level, buf):
         self.logger.debug("On Log: {0}, {1}, {2}, {3}".format(client, userdata, level, buf))
 
-    def bootstrap_mqtt(self):
+    def bootstrap_mqtt(self, config):
         self.mqttc.on_connect = self.__on_connect
         self.mqttc.on_subscribe = self.__on_subscribe
         self.mqttc.on_message = self.__on_message
         self.mqttc.on_log = self.__on_log
 
-        # awshost = "a37prab8rk99q2-ats.iot.us-east-2.amazonaws.com"  # prod
-        awshost = "a372uklyvnvbpu-ats.iot.us-east-2.amazonaws.com"  # dev
-        awsport = 8883
-        keepalive_interval = 60
+        if config['awsport'] == 443:
+            ssl_context = self.ssl_alpn(ca=config['ca'], cert=config['cert'], key=config['key'])
+            self.mqttc.tls_set_context(context=ssl_context)
+        else:
+            self.mqttc.tls_set(config['ca'],
+                               certfile=config['cert'],
+                               keyfile=config['key'],
+                               cert_reqs=ssl.CERT_REQUIRED,
+                               tls_version=ssl.PROTOCOL_TLSv1_2,
+                               ciphers=None)
 
-        ca_path = "./certs/rootCa.crt"
-        cert_path = "./certs/dev/client.crt"
-        key_path = "./certs/dev/client.key"
-
-        self.mqttc.tls_set(ca_path,
-                           certfile=cert_path,
-                           keyfile=key_path,
-                           cert_reqs=ssl.CERT_REQUIRED,
-                           tls_version=ssl.PROTOCOL_TLSv1_2,
-                           ciphers=None)
-
-        result_of_connection = self.mqttc.connect(awshost, awsport, keepalive=keepalive_interval)
+        result_of_connection = self.mqttc.connect(config['awshost'], config['awsport'], keepalive=config['keepalive'])
 
         if result_of_connection == 0:
-            self.logger.info('Connected to {0}'.format(awshost))
+            self.logger.info('Connected to {0}'.format(config['awshost']))
             self.connect = True
 
         return self
 
-    def start(self):
+    def start(self, msg):
         self.mqttc.loop_start()
         # self.mqttc.subscribe('#', 1)
 
         while True:
             sleep(2)
             if self.connect:
-                self.mqttc.publish(self.topic, mqtt_msg, qos=1)
+                self.mqttc.publish(self.topic, msg, qos=1)
             else:
                 self.logger.debug("Attempting to connect.")
 
@@ -92,8 +101,29 @@ if __name__ == '__main__':
         thing_name = get_mac_address(interface="en0").replace(':', '')
     except Exception as ex:
         print('Failed to get MAC address!\n{0}'.format(ex))
+        thing_name = '000000000000'
+        #raise
+
+    dev_env = {
+        'awshost': 'a372uklyvnvbpu-ats.iot.us-east-2.amazonaws.com',
+        'awsport': 443,
+        'keepalive': 60,
+        'ca': 'certs/dev/rootCa.crt',
+        'cert': 'certs/dev/client.crt',
+        'key': 'certs/dev/client.key'
+    }
+
+    prod_env = {
+        'awshost': 'a37prab8rk99q2-ats.iot.us-east-2.amazonaws.com',
+        'awsport': 8883,
+        'keepalive': 60,
+        'ca': 'certs/prod/rootCa.crt',
+        'cert': 'certs/prod/client.crt',
+        'key': 'certs/prod/client.key'
+    }
 
     mqtt_topic = '$aws/things/{0}/publish'.format(thing_name)
     mqtt_msg = 'Test Publish: {0}'.format(thing_name)
+
     print("Running...")
-    PubSub(listener=True, topic=mqtt_topic, mqtt_client_id=thing_name).bootstrap_mqtt().start()
+    PubSub(listener=True, topic=mqtt_topic, mqtt_client_id=thing_name).bootstrap_mqtt(dev_env).start(mqtt_msg)
